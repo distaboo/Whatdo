0695595860fd57d29967f81398c8b0a9a0430797
Denis Gryaznov <distaboo@mail.ru>
2017-02-27 14:03:26 +0600

    Начало

---
+++ app/src/main/java/ru/invapp/horo/ListFragment.java
@@ -0,0 +1,32 @@
+package ru.invapp.horo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by User on 27.02.2017.
+ */
+
+public class ListFragment extends Fragment {
+
+    TextView text;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_content, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        text =(TextView) view.findViewById(R.id.text);
+        text.setText("Хуууй");
+    }
+}
---
+++ app/src/main/java/ru/invapp/horo/MainActivity.java
@@ -0,0 +1,56 @@
+package ru.invapp.horo;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    private void findViews() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews();
+        setSupportActionBar(toolbar);
+
+        //create and set ViewPager adapter
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        setupViewPager(viewPager);
+
+
+
+        //attach tab layout with ViewPager
+        //set gravity for tab bar
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+
+        //change selected tab when viewpager changed page
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ContentFragment(), "Что делать");
+        adapter.addFragment(new ContentFragment(), "Список");
+        adapter.addFragment(new ListFragment(), "Настройки");
+        viewPager.setAdapter(adapter);
+    }
+}
